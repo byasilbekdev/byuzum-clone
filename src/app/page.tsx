de4b9925d58d542311b631d35409a1e2193fa70b
Asilbek Egamnazarov <byasilbek9@gmail.com>
2026-07-15 20:32:53 +0500

    git add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { Card } from "@/components/ui/card";
+
 const page = () => {
-  return <div>page</div>;
+  return (
+    <Card className="glass glass-hover p-6 flex items-center justify-center flex-col gap-4 w-100 h-100">
+      hello
+    </Card>
+  );
 };
 
 export default page;
